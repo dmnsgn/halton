c18737b1bd2c4fb63b7cdc2b2bb22447700a998d
Damien Seguin <dmnsgn@gmail.com>
2025-02-05 13:45:06 +0000

    docs: add haltonSequence

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+/** @module halton */
+
 import { radicalInverse } from "hammersley";
 
-// Based on pseudocode from https://en.wikipedia.org/wiki/Halton_sequence#Implementation
+/**
+ * The Halton sequence algorithm.
+ * @see [Wikipedia Halton_sequence]{@link https://en.wikipedia.org/wiki/Halton_sequence#Implementation}
+ * @param {number} index Index in the sequence.
+ * @param {number} base An integer, preferably prime number.
+ * @returns {number}
+ */
 function haltonSequence(index, base) {
   let fraction = 1;
   let result = 0;
@@ -17,6 +25,7 @@ function haltonSequence(index, base) {
 /**
  * Generate n points of the Halton sequence in n-dimensions.
  * The "bases" parameter defines how many dimensions.
+ * @alias module:halton.default
  * @param {number} count Number of points in the sequence to generate.
  * @param {number[]} [bases=[2, 3]] The bases for the Van der Corput operation. Best results with small coprime numbers.
  * @returns {number[][]}
