2b38dd50199345c5506fc5d12ee48a1b8c3ede75
Damien Seguin <dmnsgn@gmail.com>
2025-02-05 13:33:11 +0000

    perf: use optimised radical inverse from hammersley for base 2 directly
    - show some safe co-prime pairs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import hammersley from "hammersley";
+import { radicalInverse } from "hammersley";
 
+// Based on pseudocode from https://en.wikipedia.org/wiki/Halton_sequence#Implementation
 function haltonSequence(index, base) {
   let fraction = 1;
   let result = 0;
@@ -23,10 +24,7 @@ function haltonSequence(index, base) {
 const halton = (count, bases = [2, 3]) =>
   Array.from({ length: count }, (_, index) =>
     bases.map((base) =>
-      base === 2
-        ? // ? radicalInverse(index)
-          hammersley(index, 1)[1]
-        : haltonSequence(index, base),
+      base === 2 ? radicalInverse(index) : haltonSequence(index, base),
     ),
   );
 
